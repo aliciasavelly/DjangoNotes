da7a7316ba8a758a4f5b60cd0ff7a6a5a098f285
aliciasavelly <alicia.savelly@gmail.com>
2017-09-01 21:58:33 -0700

    allow full user signup/ sign in / logout to work

--- NotesApp/urls.py
+++ NotesApp/urls.py
@@ -16,10 +16,12 @@ Including another URLconf
 from django.conf.urls import include, url
 from django.contrib import admin
 from django.contrib.auth import views as auth_views
+from notes import views as core_views
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^logout/$', auth_views.logout, name='logout'),
+    url(r'^logout/$', auth_views.logout, {'next_page': '/login'}, name='logout'),
     url(r'^login/$', auth_views.login, name='login'),
     url(r'', include('notes.urls')),
+    url(r'^signup/$', core_views.signup, name='signup'),
 ]
